7b02ab379be0ee24011c7f19dfda1a876d5fc2ce
Derek Kershner <dkershner@dkershner.com>
2021-10-26 15:24:00 +0000

    lint

--- src/hooks/useForgotPassword.ts
+++ src/hooks/useForgotPassword.ts
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import { useContext, useState } from "react";
 
 import { Auth } from "@aws-amplify/auth";
 import invariant from "tiny-invariant";
