62b141c53c285b347429ce6a85a13390fe2b70cf
Derek Kershner <dkershner@dkershner.com>
2021-10-26 17:45:49 +0000

    useCheckContact tested

--- src/hooks/useAuthRoute.test.ts
+++ src/hooks/useAuthRoute.test.ts
@@ -1,17 +1,17 @@
-import { renderHook, act } from "@testing-library/react-hooks";
+import { renderHook } from "@testing-library/react-hooks";
 
 import { AuthRoute, useAuthRoute } from "..";
 import TestWrapper, { dispatchAuthState } from "../test/TestWrapper";
 
 describe("useAuthRoute", () => {
     it("Should pass along props to dispatchAuthState", () => {
-        const { result, rerender } = renderHook(useAuthRoute, {
+        const { result } = renderHook(useAuthRoute, {
             wrapper: TestWrapper,
         });
 
         const testRoute = AuthRoute.SignUp;
         result.current(testRoute);
-        rerender();
+
         expect(dispatchAuthState).toHaveBeenCalledWith({
             authRoute: testRoute,
         });
