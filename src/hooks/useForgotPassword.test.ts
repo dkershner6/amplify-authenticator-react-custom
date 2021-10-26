f20bb4b21bb350baaf9d62ff27674372beaf495a
Derek Kershner <dkershner@dkershner.com>
2021-10-26 18:45:11 +0000

    useForgotPassword tested

---
+++ src/hooks/useForgotPassword.test.ts
@@ -0,0 +1,73 @@
+import { Auth } from "@aws-amplify/auth";
+import { renderHook, act } from "@testing-library/react-hooks";
+import { mocked } from "ts-jest/utils";
+
+import { AuthRoute } from "..";
+import TestWrapper, { dispatchAuthState } from "../test/TestWrapper";
+
+import { useForgotPassword } from "./useForgotPassword";
+
+jest.mock("@aws-amplify/auth");
+
+describe("useForgotPassword", () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    const testUsername = "testUser";
+    const testCode = "1234";
+    const testPassword = "P@ssw0rd";
+
+    it("Should call Auth with correct params for send", async () => {
+        const { result } = renderHook(useForgotPassword, {
+            wrapper: TestWrapper,
+        });
+
+        await act(() => result.current.send(testUsername));
+
+        expect(mocked(Auth.forgotPassword)).toHaveBeenCalledWith(testUsername);
+    });
+
+    it("Should populate username on send", async () => {
+        const { result, waitFor } = renderHook(useForgotPassword, {
+            wrapper: TestWrapper,
+        });
+
+        await act(() => result.current.send(testUsername));
+
+        await waitFor(() => {
+            expect(result.current.username).toEqual(testUsername);
+        });
+    });
+
+    it("Should call Auth with correct params on submit", async () => {
+        const { result } = renderHook(useForgotPassword, {
+            wrapper: TestWrapper,
+        });
+
+        await act(() => result.current.send(testUsername));
+
+        await act(() => result.current.submit(testCode, testPassword));
+
+        expect(mocked(Auth.forgotPasswordSubmit)).toHaveBeenCalledWith(
+            testUsername,
+            testCode,
+            testPassword
+        );
+    });
+
+    it("Should route to SignIn when complete", async () => {
+        const { result } = renderHook(useForgotPassword, {
+            wrapper: TestWrapper,
+        });
+
+        await act(() => result.current.send(testUsername));
+
+        await act(() => result.current.submit(testCode, testPassword));
+
+        expect(dispatchAuthState).toHaveBeenCalledWith({
+            authRoute: AuthRoute.SignIn,
+            authData: null,
+        });
+    });
+});
